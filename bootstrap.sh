9ca10c38298c828de601f3c57b21f2fe7d1c5eb4
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2015-02-28 12:21:50 -0500

    Gtest too.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 SRC=https://raw.githubusercontent.com/starcraftman/.my_scripts/master/archive/GetLibsI.py
 curl -o GetLibsI.py $SRC
-./GetLibsI.py boost
+./GetLibsI.py boost gtest
 rm GetLibsI.py
