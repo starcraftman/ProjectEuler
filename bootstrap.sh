2c4caa2bf872d2b81b2e73e971e91cab36c54004
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2015-03-01 11:43:47 -0500

    Typo.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 SRC=https://raw.githubusercontent.com/starcraftman/.my_scripts/master/archive/GetLibsI.py
 curl -o GetLibsI.py $SRC
-python GetLibsI.py boost gmp gtest
+python GetLibsI.py boost gnump gtest
 rm GetLibsI.py
