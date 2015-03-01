7f5c7804dfa4a40a89a563f6ce79596e3222c9dd
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2015-03-01 11:41:05 -0500

    May use gmp later.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 SRC=https://raw.githubusercontent.com/starcraftman/.my_scripts/master/archive/GetLibsI.py
 curl -o GetLibsI.py $SRC
-python GetLibsI.py boost gtest
+python GetLibsI.py boost gmp gtest
 rm GetLibsI.py
