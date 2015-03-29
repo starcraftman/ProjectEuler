2ca4fcf9c3439ec8e41b242d1c0f9398e36d4b9f
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2015-03-29 13:30:49 -0400

    Description.

--- cpp/src/problem004.cpp
+++ cpp/src/problem004.cpp
@@ -1,82 +1,18 @@
 /**
- * Template C++ file.
- * Function with references to pointers: func(int *&ptr);
+A palindromic number reads the same both ways. The largest palindrome made
+from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
  */
 /********************* Header Files ***********************/
 /* C++ Headers */
 #include <iostream> /* Input/output objects. */
-//#include <fstream> /* File operations. */
-//#include <sstream> /* String stream. */
-#include <string> /* C++ String class. */
-//#include <exception> /* Top level exception header. */
-//#include <stdexcept> /* Some useful common exceptions */
-//#include <new> /* Defines bad_malloc exception, new functions. */
-//#include <memory> /* std smart pointers like unique_ptr */
-//#include <typeinfo> /* Casting header. */
-//#include <limits> /* Inspect properties, numeric_limits<int> iLimit; */
-//#include <numeric> /* Math accumulators & other ops*/
-//#include <functional> /* Function declarations & std::bind */
-
-/* C++11 Only*/
-//#include <chrono> /* std::chrono contains timing for threads */
-//#include <initializer_list> /* Allows class to take list like {1,2} */
-//#include <random> /* Random c++ version */
-//#include <regex>
-//#include <thread>
-//#include <atomic>
-//#include <condition_variable>
-//#include <mutex>
-
-/* STL Headers */
-//#include <vector>
-//#include <list>
-//#include <deque>
-//#include <stack>
-//#include <queue>
-//#include <priority_queue>
-//#include <set> // multiset for multiple keys allowed.
-//#include <map> // multimap for multiple keys allowed.
-//#include <bitset>
-//#include <utility> // Has pair for map, std::swap
-//#include <algorithm>
-//#include <iterator> // Contains back_inserter function and like.
-
-/* C Headers */
-//#include <cstdio>
-//#include <cstring>
-//#include <cstdlib> /* atof, rand, malloc... */
-//#include <cstddef> /* size_t, NULL */
-//#include <cstdarg> /* Variable argument functions */
-//#include <cctype> /* Character check functions */
-//#include <climits>
-//#include <cassert>
-//#include <cmath>
-//#include <cstdint> /* C++11 only, standard u_int16 & such */
 
 #include "gtest/gtest.h"
 
 /**************** Namespace Declarations ******************/
 using std::cout;
 using std::endl;
-
-/******************* Type Definitions *********************/
-/* For enums: Try to namesapce the common elements.
- * typedef enum {
- *	VAL_,
- * } name_e;
- */
-
-/* For structs:
- * typedef struct name_s {
- *	int index;
- * } name_t;
- */
-
-/***************** Constants & Macros *********************/
-
-
-/****************** Class Definitions *********************/
-
 
 /************** Global Vars & Functions *******************/
 int reverse(int num) {
