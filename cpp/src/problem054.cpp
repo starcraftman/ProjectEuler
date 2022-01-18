0a236eba11ea7aa3fdf19c7fd5a8b0c1470877ec
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2022-01-18 18:27:37 -0500

    Implement hand value detection.
    - Still missing ability to break ties & compare hands directly.

--- cpp/src/problem054.cpp
+++ cpp/src/problem054.cpp
@@ -35,10 +35,10 @@ How many hands does Player 1 win?
 #include <fstream>
 #include <sstream>
 #include <exception>
+#include <initializer_list>
 #include <map>
 #include <set>
 #include <algorithm>
-#include <initializer_list>
 
 #include "gtest/gtest.h"
 #include "util.hpp"
@@ -48,9 +48,8 @@ using std::cout;
 using std::endl;
 
 /************** Global Vars & Functions *******************/
-typedef std::int8_t num_t;
+typedef int num_t;
 static const std::string INPUT = "./src/input_e054.txt";
-static const std::string FIRST_DEAL = "8C TS KC 9H 4S 7D 2S 5D 3S AC";
 
 enum Suits {
     Hearts,
@@ -59,7 +58,7 @@ enum Suits {
     Spades,
 };
 enum HandTypes {
-    HighCard = 1,
+    HighCard,
     OnePair,
     TwoPair,
     ThreeKind,
@@ -125,27 +124,12 @@ static std::map<HandTypes, std::string> hand_type_to_text = {
     {HandTypes::RoyalFlush, "Royal Flush"},
 };
 
-static const std::string HAND_HIGH_CARD = "2H 2D 4S 5C KC";
-static const std::string HAND_ONE_PAIR = "2H 2D 4S 5C 9C";
-static const std::string HAND_TWO_PAIR = "2H 2D 5C 5S 9C";
-static const std::string HAND_THREE_KIND = "2H 2D 2C 5C 9C";
-static const std::string HAND_STRAIGHT = "2H 3D 4S 5C 6C";
-static const std::string HAND_FLUSH = "2H 6H 9H QH AH";
-static const std::string HAND_FULL_HOUSE = "JH JD JC QH QD";
-static const std::string HAND_FOUR_KIND = "2H 2D 2C 2S 9C";
-static const std::string HAND_STRAIGHT_FLUSH = "5H 6H 7H 8H 9H";
-static const std::string HAND_ROYAL_FLUSH = "TH JH QH KH AH";
-
 class Card
 {
 public:
     Card () : value(2), suit(Suits::Hearts) {};
     explicit Card(const std::string &text) {
         this->change(text);
-    }
-    Card(const Card &other) {
-        this->value = other.value;
-        this->suit = other.suit;
     }
     // Constraint: Text is valid. I won't validate. Input guaranteed no dupes nor invalid.
     void change(const std::string &text) {
@@ -194,18 +178,19 @@ public:
     bool selected = false;
 };
 
+// A possible subset of cards, like three of a kind or two pair, or whole sets like royal flush
 class CardGroup {
 public:
-    CardGroup() : type(HandTypes::HighCard) {}
+    CardGroup(HandTypes type = HandTypes::HighCard) : type(type) {}
     explicit CardGroup(const Card &card) : type(HandTypes::HighCard) {
         this->cards.push_back(card);
     }
     // Returns true only if card taken by this group
-    bool check_card(const Card &next_card) {
+    bool check_card(Card &next_card) {
         if (this->cards.size() != 0) {
             Card last = this->cards.back();
             if (last.value == next_card.value) {
-                this->cards.push_back(next_card);
+                this->add_card(next_card);
                 this->set_type();
                 return true;
             }
@@ -213,8 +198,22 @@ public:
 
         return false;
     }
-    void add_card(const Card &next_card) {
+    // Adding a card has side effects:
+    // - Will select the card signalling usage beyond "High Card"
+    // - Will set type for the group, based on count of same value cards.
+    CardGroup& add_card(Card &next_card) {
+        next_card.selected = true;
         this->cards.push_back(next_card);
+        this->set_type();
+        return *this;
+    }
+    template <class Iterator>
+    CardGroup& add_all(Iterator begin, Iterator end) {
+        this->cards.insert(this->cards.end(), begin, end);
+        return *this;
+    }
+    std::size_t size() {
+        return this->cards.size();
     }
     void set_type() {
         switch (cards.size()) {
@@ -273,6 +272,38 @@ class HandValue {
 public:
     HandValue(HandTypes type = HandTypes::HighCard, int value = 2) :
         type(type), type_value(value) {};
+    void add_group(const CardGroup &group) {
+        groups.push_back(group);
+        if (group.type > this->type ||
+            (group.type == this->type && group.value() > this->type_value)) {
+            this->type = group.type;
+            this->type_value = group.value();
+        }
+
+        this->check_composite_types();
+    }
+    void check_composite_types() {
+        if (this->groups.size() == 2) {
+            int num_pairs = 0;
+            int num_triples = 0;
+            for (auto group : this->groups) {
+                if (group.type == HandTypes::OnePair) {
+                    num_pairs++;
+                } else if (group.type == HandTypes::ThreeKind) {
+                    num_triples++;
+                }
+            }
+
+            if (num_pairs == 2) {
+                this->type = HandTypes::TwoPair;
+            } else if (num_pairs == 1 && num_triples == 1) {
+                this->type = HandTypes::FullHouse;
+            }
+        }
+    }
+    bool is_set() {
+        return this->groups.size() != 0;
+    }
 
     friend std::ostream& operator<<(std::ostream &os, const Card &card);
 
@@ -288,6 +319,7 @@ public:
     void sort() {
         std::stable_sort(this->cards.begin(), this->cards.end());
     }
+    void detect_hand_value();
 
     bool operator==(const Hand &other) const {
         if (this->cards.size() != other.cards.size()) {
@@ -326,11 +358,12 @@ std::istream& Hand::read_cards(std::istream &is) {
 
     auto iter = cards.begin();
     int cnt = 5;
-    while (iter != cards.end() && is.good() && cnt-- != 0) {
+    while (iter != cards.end() && is && cnt-- != 0) {
         is >> *iter;
         iter++;
     }
     this->sort();
+    this->value = HandValue();
 
     return is;
 }
@@ -350,8 +383,23 @@ std::ostream& operator<<(std::ostream &os, const Hand &hand) {
 }
 
 std::ostream& operator<<(std::ostream &os, const CardGroup &group) {
-    os << "Card Group: " << hand_type_to_text[group.type] << " of "
-        << group.cards.back().value << "s.";
+    os << "Group: " << hand_type_to_text[group.type] << " ( ";
+    for (auto card : group.cards) {
+        os << card << " ";
+    }
+    os << ")";
+    return os;
+}
+
+std::ostream& operator<<(std::ostream &os, const HandValue &hand_value) {
+    os << "Value of hand: " << hand_type_to_text[hand_value.type] << " of "
+        << hand_value.type_value << endl;
+
+    os << "Has following card groupings:" << endl;
+    for (auto group : hand_value.groups) {
+        os << group << endl;
+    }
+    return os;
 }
 
 std::istream& operator>>(std::istream &is, Card &card) {
@@ -383,7 +431,10 @@ bool detect_royal_flush(Hand &hand) {
         }
     }
 
-    hand.value = HandValue(HandTypes::RoyalFlush, iter->value);
+    hand.value.add_group(
+        CardGroup(HandTypes::RoyalFlush).add_all(
+        hand.cards.begin(), hand.cards.end())
+    );
     return true;
 }
 
@@ -398,7 +449,27 @@ bool detect_straight_flush(Hand &hand) {
         }
     }
 
-    hand.value = HandValue(HandTypes::StraightFlush, iter->value);
+    hand.value.add_group(
+        CardGroup(HandTypes::StraightFlush).add_all(
+        hand.cards.begin(), hand.cards.end())
+    );
+    return true;
+}
+
+bool detect_flush(Hand &hand) {
+    auto iter = hand.cards.cbegin();
+    Card expect(*iter);
+
+    while (++iter != hand.cards.cend()) {
+        if (iter->suit != expect.suit) {
+            return false;
+        }
+    }
+
+    hand.value.add_group(
+        CardGroup(HandTypes::Flush).add_all(
+        hand.cards.begin(), hand.cards.end())
+    );
     return true;
 }
 
@@ -413,38 +484,119 @@ bool detect_straight(Hand &hand) {
         }
     }
 
-    hand.value = HandValue(HandTypes::Straight, iter->value);
+    hand.value.add_group(
+        CardGroup(HandTypes::Straight).add_all(
+        hand.cards.begin(), hand.cards.end())
+    );
     return true;
 }
 
-bool detect_flush(Hand &hand) {
+// Detects the following:
+//      four of a kind, full house, three of a kind, two pair, one pair,
+bool detect_pairs(Hand &hand) {
+    CardGroup current_group;
+    auto iter = hand.cards.begin();
+    Card last_card(*iter);
+
+    while (++iter != hand.cards.end()) {
+        // Different card seen
+        if (iter->value != last_card.value) {
+            if (current_group.size() >= 2) {
+                hand.value.add_group(current_group);
+            }
+            current_group = CardGroup();
+        }
+
+        // Last two cards or more same, bunch in group
+        if (iter->value == last_card.value) {
+            if (current_group.size() == 0) {
+                current_group.add_card(last_card);
+            }
+            current_group.check_card(*iter);
+        }
+
+        last_card = Card(*iter);
+    }
+
+    if (current_group.size() >= 2) {
+        hand.value.add_group(current_group);
+    }
+
+    return hand.value.groups.size() != 0;
+}
+
+bool detect_high_card(Hand &hand) {
     auto iter = hand.cards.cbegin();
-    Card expect(*iter);
+    Card highest(*iter);
 
     while (++iter != hand.cards.cend()) {
-        if (iter->suit != expect.suit) {
-            return false;
+        if (iter->value > highest.value) {
+            highest = Card(*iter);
         }
     }
 
-    hand.value = HandValue(HandTypes::Flush, iter->value);
+    hand.value.add_group(
+        CardGroup(HandTypes::HighCard).add_card(highest)
+    );
     return true;
 }
 
-// TODO: Integrate with above dtection, assign n hands a value, highest value wins.
-//       Value should take into account ties.
-void assign_hand_value(Hand &hand) {
-
+// Types for holding the detection functions
+typedef bool func_detect_t(Hand &);
+typedef func_detect_t* pfunc_detect;
+// Run through detection list from highest value to least.
+void Hand::detect_hand_value() {
+    std::initializer_list<pfunc_detect> func_ptrs = {
+        detect_royal_flush,
+        detect_straight_flush,
+        detect_flush,
+        detect_straight,
+        detect_pairs,
+        detect_high_card,
+    };
+    for (auto fptr : func_ptrs) {
+        if (fptr(*this)) {
+            break;
+        }
+    }
 }
-
 
 int number_won_hands() {
-    Suits suit = Suits::Hearts;
-    cout << suit << endl;
+    int player_1_won = 0;
 
+    Hand hand1(1), hand2(2);
+    try {
+        std::ifstream input(INPUT, std::ifstream::in);
+        while (input) {
+            input >> hand1 >> hand2;
+            hand1.detect_hand_value();
+            hand2.detect_hand_value();
+            cout << hand1 << endl << hand1.value << endl
+                << hand2 << endl << hand2.value << endl;
 
-    return 0;
+            // TODO: Compare hand values
+        }
+    } catch (std::exception &e) {
+        // Input gone bad, ignore.
+    }
+
+    return player_1_won;
 }
+
+////////////
+// Test Code
+////////////
+static const std::string FIRST_DEAL = "8C TS KC 9H 4S 7D 2S 5D 3S AC";
+static const std::string HAND_HIGH_CARD = "2H 2D 4S 5C KC";
+static const std::string HAND_ONE_PAIR = "2H 2D 4S 5C 9C";
+static const std::string HAND_TWO_PAIR = "2H 2D 5C 5S 9C";
+static const std::string HAND_THREE_KIND = "2H 2D 2C 5C 9C";
+static const std::string HAND_STRAIGHT = "2H 3D 4S 5C 6C";
+static const std::string HAND_FLUSH = "2H 6H 9H QH AH";
+static const std::string HAND_FULL_HOUSE = "JH JD JC QH QD";
+static const std::string HAND_FOUR_KIND = "2H 2D 2C 2S 9C";
+static const std::string HAND_STRAIGHT_FLUSH = "5H 6H 7H 8H 9H";
+static const std::string HAND_ROYAL_FLUSH = "TH JH QH KH AH";
 
 TEST(E054_Card, CardDefault) {
     Card deuce;
@@ -516,115 +668,167 @@ TEST(E054_Card, CardToText) {
 }
 
 TEST(E054_Hand, HandCreation) {
-    Hand hand1;
-    ASSERT_EQ(hand1.player, 1);
-    ASSERT_EQ(hand1.cards.size(), 0);
+    Hand hand;
+    ASSERT_EQ(hand.player, 1);
+    ASSERT_EQ(hand.cards.size(), 0);
 }
 
 TEST(E054_Hand, HandSortValues) {
-    Hand hand1;
-    std::stringstream(FIRST_DEAL) >> hand1;
-    hand1.sort();
+    Hand hand;
+    std::stringstream(FIRST_DEAL) >> hand;
+    hand.sort();
     std::stringstream ss;
-    ss << hand1;
+    ss << hand;
     std::string expect("Player 1 has: 4S 8C 9H TS KC ");
     ASSERT_EQ(ss.str(), expect);
 }
 
 TEST(E054_Hand, HandSortValueAndSuit) {
-    Hand hand1;
-    std::stringstream("KD JS KS JH JD") >> hand1;
-    hand1.sort();
+    Hand hand;
+    std::stringstream("KD JS KS JH JD") >> hand;
+    hand.sort();
     std::stringstream ss;
-    ss << hand1;
+    ss << hand;
     std::string expect("Player 1 has: JH JD JS KD KS ");
     ASSERT_EQ(ss.str(), expect);
 }
 
+TEST(E054_Hand, HandDetectHandValue) {
+    Hand hand;
+    std::stringstream(HAND_STRAIGHT) >> hand;
+    hand.detect_hand_value();
+    ASSERT_EQ(hand.value.type, HandTypes::Straight);
+}
+
 TEST(E054_Hand, HandInputOperator) {
-    Hand hand1;
-    std::stringstream(FIRST_DEAL) >> hand1;
-    ASSERT_EQ(hand1.player, 1);
-    ASSERT_EQ(hand1.cards[0], Card("4S"));
+    Hand hand;
+    std::stringstream(FIRST_DEAL) >> hand;
+    ASSERT_EQ(hand.player, 1);
+    ASSERT_EQ(hand.cards[0], Card("4S"));
 }
 
 TEST(E054_Hand, HandOutputOperator) {
-    Hand hand1;
-    std::stringstream(FIRST_DEAL) >> hand1;
+    Hand hand;
+    std::stringstream(FIRST_DEAL) >> hand;
     std::stringstream ss;
-    ss << hand1;
+    ss << hand;
     std::string expect("Player 1 has: 4S 8C 9H TS KC ");
     ASSERT_EQ(ss.str(), expect);
 }
 
 TEST(E054_CardGroup, AddCard) {
     CardGroup group;
-    group.add_card(Card("8H"));
+    Card card("8H");
+    group.add_card(card);
     ASSERT_EQ(group.cards.size(), 1);
     ASSERT_EQ(group.type, HandTypes::HighCard);
 }
 
 TEST(E054_CardGroup, CheckCardValid) {
     CardGroup group;
-    group.add_card(Card("8H"));
-    ASSERT_TRUE(group.check_card(Card("8D")));
+    Card card("8H");
+    group.add_card(card);
+    card = Card("8D");
+    ASSERT_TRUE(group.check_card(card));
     ASSERT_EQ(group.type, HandTypes::OnePair);
     ASSERT_EQ(group.cards.back(), Card("8D"));
 
-    ASSERT_TRUE(group.check_card(Card("8C")));
+    card = Card("8C");
+    ASSERT_TRUE(group.check_card(card));
     ASSERT_EQ(group.type, HandTypes::ThreeKind);
     ASSERT_EQ(group.cards.back(), Card("8C"));
 
-    ASSERT_TRUE(group.check_card(Card("8S")));
+    card = Card("8S");
+    ASSERT_TRUE(group.check_card(card));
     ASSERT_EQ(group.type, HandTypes::FourKind);
     ASSERT_EQ(group.cards.back(), Card("8S"));
 }
 
 TEST(E054_CardGroup, CheckCardInvalid) {
     CardGroup group;
-    group.add_card(Card("8H"));
-    ASSERT_FALSE(group.check_card(Card("7D")));
+    Card card = Card("8H");
+    group.add_card(card);
+    card = Card("7D");
+    ASSERT_FALSE(group.check_card(card));
     ASSERT_EQ(group.type, HandTypes::HighCard);
     ASSERT_EQ(group.cards.back(), Card("8H"));
 }
 
 TEST(E054_CardGroup, SetType) {
     CardGroup group;
-    group.add_card(Card("8H"));
-    group.add_card(Card("8D"));
-    group.set_type();
+    Card card = Card("8H");
+    group.add_card(card);
+    card = Card("8D");
+    group.add_card(card);
     ASSERT_EQ(group.type, HandTypes::OnePair);
 
-    group.add_card(Card("8C"));
-    group.set_type();
+    card = Card("8C");
+    group.add_card(card);
     ASSERT_EQ(group.type, HandTypes::ThreeKind);
 
-    group.add_card(Card("8S"));
-    group.set_type();
+    card = Card("8S");
+    group.add_card(card);
     ASSERT_EQ(group.type, HandTypes::FourKind);
+}
+
+TEST(E054_CardGroup, CardGroupOutputOperator) {
+    CardGroup group;
+    Card card = Card("8H");
+    group.add_card(card);
+    card = Card("8D");
+    group.add_card(card);
+    card = Card("8C");
+    group.add_card(card);
+    card = Card("8S");
+    group.add_card(card);
+    ASSERT_EQ(group.type, HandTypes::FourKind);
+
+    std::stringstream ss;
+    ss << group;
+    ASSERT_EQ(ss.str(), std::string("Group: Four of a kind ( 8H 8D 8C 8S )"));
 }
 
 // To debug detectors ....
 //cout << "Read hand" << endl << hand << endl << hand2 << endl;
-TEST(E054_HandValue, RoyalFlush) {
+TEST(E054_DetectCards, HighCard) {
     Hand hand(1);
-    Hand hand2(2);
-    std::stringstream ss(HAND_ROYAL_FLUSH + " " + HAND_STRAIGHT);
-    ss >> hand >> hand2;
-    ASSERT_TRUE(detect_royal_flush(hand));
-    ASSERT_FALSE(detect_royal_flush(hand2));
+    std::stringstream ss(HAND_HIGH_CARD);
+    ss >> hand;
+    ASSERT_TRUE(detect_high_card(hand));
+    ASSERT_EQ(hand.value.type_value, face_to_value["K"]);
 }
 
-TEST(E054_HandValue, StraightFlush) {
+TEST(E054_DetectCards, OnePair) {
     Hand hand(1);
     Hand hand2(2);
-    std::stringstream ss(HAND_STRAIGHT_FLUSH + " " + HAND_STRAIGHT);
+    std::stringstream ss(HAND_ONE_PAIR + " " + HAND_STRAIGHT);
     ss >> hand >> hand2;
-    ASSERT_TRUE(detect_straight_flush(hand));
-    ASSERT_FALSE(detect_straight_flush(hand2));
+    ASSERT_TRUE(detect_pairs(hand));
+    ASSERT_EQ(hand.value.type, HandTypes::OnePair);
+    ASSERT_FALSE(detect_pairs(hand2));
 }
 
-TEST(E054_HandValue, Straight) {
+TEST(E054_DetectCards, TwoPair) {
+    Hand hand(1);
+    Hand hand2(2);
+    std::stringstream ss(HAND_TWO_PAIR + " " + HAND_STRAIGHT);
+    ss >> hand >> hand2;
+    ASSERT_TRUE(detect_pairs(hand));
+    ASSERT_EQ(hand.value.type, HandTypes::TwoPair);
+    ASSERT_FALSE(detect_pairs(hand2));
+}
+
+TEST(E054_DetectCards, ThreeKind) {
+    Hand hand(1);
+    Hand hand2(2);
+    std::stringstream ss(HAND_THREE_KIND + " " + HAND_STRAIGHT);
+    ss >> hand >> hand2;
+    ASSERT_TRUE(detect_pairs(hand));
+    ASSERT_EQ(hand.value.type, HandTypes::ThreeKind);
+    ASSERT_FALSE(detect_pairs(hand2));
+}
+
+TEST(E054_DetectCards, Straight) {
     Hand hand(1);
     Hand hand2(2);
     std::stringstream ss(HAND_STRAIGHT + " " + HAND_FLUSH);
@@ -633,7 +837,7 @@ TEST(E054_HandValue, Straight) {
     ASSERT_FALSE(detect_straight(hand2));
 }
 
-TEST(E054_HandValue, Flush) {
+TEST(E054_DetectCards, Flush) {
     Hand hand(1);
     Hand hand2(2);
     std::stringstream ss(HAND_FLUSH + " " + HAND_STRAIGHT);
@@ -642,23 +846,45 @@ TEST(E054_HandValue, Flush) {
     ASSERT_FALSE(detect_flush(hand2));
 }
 
-// TODO: Solution loop, use at end
-// TEST(E054_Hand, ReadPokerHands) {
-    // cout << "Read hand" << endl;
-    // std::ifstream input(INPUT, std::ifstream::in);
-    // Hand hand1(1);
-    // Hand hand2(2);
-    // try {
-        // while (input.good()) {
-            // input >> hand1 >> hand2;
-            // cout << hand1 << endl << hand2 << endl;
-        // }
-    // } catch (std::exception &e) {
-        // // Input gone bad, ignore.
-    // }
-// }
+TEST(E054_DetectCards, FullHouse) {
+    Hand hand(1);
+    Hand hand2(2);
+    std::stringstream ss(HAND_FULL_HOUSE + " " + HAND_STRAIGHT);
+    ss >> hand >> hand2;
+    ASSERT_TRUE(detect_pairs(hand));
+    ASSERT_EQ(hand.value.type, HandTypes::FullHouse);
+    ASSERT_FALSE(detect_pairs(hand2));
+}
 
-// TEST(Euler054, PokerHandsWon) {
+TEST(E054_DetectCards, FourKind) {
+    Hand hand(1);
+    Hand hand2(2);
+    std::stringstream ss(HAND_FOUR_KIND + " " + HAND_STRAIGHT);
+    ss >> hand >> hand2;
+    ASSERT_TRUE(detect_pairs(hand));
+    ASSERT_EQ(hand.value.type, HandTypes::FourKind);
+    ASSERT_FALSE(detect_pairs(hand2));
+}
+
+TEST(E054_DetectCards, StraightFlush) {
+    Hand hand(1);
+    Hand hand2(2);
+    std::stringstream ss(HAND_STRAIGHT_FLUSH + " " + HAND_STRAIGHT);
+    ss >> hand >> hand2;
+    ASSERT_TRUE(detect_straight_flush(hand));
+    ASSERT_FALSE(detect_straight_flush(hand2));
+}
+
+TEST(E054_DetectCards, RoyalFlush) {
+    Hand hand(1);
+    Hand hand2(2);
+    std::stringstream ss(HAND_ROYAL_FLUSH + " " + HAND_STRAIGHT);
+    ss >> hand >> hand2;
+    ASSERT_TRUE(detect_royal_flush(hand));
+    ASSERT_FALSE(detect_royal_flush(hand2));
+}
+
+// TEST(Euler054, NumberWonHands) {
     // int result = number_won_hands();
     // cout << "The number of hands player 1 won is: " << result << endl;
 // }
